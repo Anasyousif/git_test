7e799365dd21086fdc8b734181363bee0d876ef0
Anas Yousif <Anasyousif182@gmail.com>
2026-04-17 16:45:01 -0400

    tail

---
+++ tail_linked.py
@@ -0,0 +1,35 @@
+from node import Node
+
+
+class LinkedList:
+    def add_to_head(self, node):
+        # 1. Point the new node to the current first node (the head)
+        node.set_next(self.head)
+        # 2. Update the list's head to be this new node
+        self.head = node
+
+    # don't touch below this line
+
+    def add_to_tail(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        last_node = None
+        for current_node in self:
+            last_node = current_node
+        last_node.set_next(node)
+
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " -> ".join(nodes)
