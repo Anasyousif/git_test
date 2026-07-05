6d74cc15711291b02ca307930f70b2ee57d8dbb5
Anas Yousif <Anasyousif182@gmail.com>
2026-07-05 13:55:59 -0400

    strings

---
+++ snekObjects.c
@@ -0,0 +1,17 @@
+#ifndef SNEKOBJECT_H
+#define SNEKOBJECT_H
+
+typedef enum {
+  INTEGER
+} snek_object_kind_t;
+
+typedef union {
+  int v_int;
+} snek_object_data_t;
+
+typedef struct {
+  snek_object_kind_t kind; 
+  snek_object_data_t data; 
+} snek_object_t; 
+
+#endif
