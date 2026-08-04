c59586ce58b49353370841447bb6dc7fead80249
Anas Yousif <Anasyousif182@gmail.com>
2026-08-04 12:29:13 -0400

    mulit

---
+++ mulitple_inter.go
@@ -0,0 +1,30 @@
+package main
+import "fmt"
+
+func (e email) cost() int {
+	if !e.isSubscribed{
+		return len(e.body) * 5
+	} 
+	return len(e.body) *2
+}
+
+func (e email) format() string {
+	status := "Not Subscribed"
+	if e.isSubscribed {
+		status = "Subscribed"
+	}
+	return fmt.Sprintf("'%s' | %s",e.body,status)
+}
+
+type expense interface {
+	cost() int
+}
+
+type formatter interface {
+	format() string
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+}
