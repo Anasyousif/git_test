cd8245ef8d24af786ec537ff02037cf8ce217dc0
Anas Yousif <Anasyousif182@gmail.com>
2026-08-12 08:54:48 -0400

    loop

---
+++ git_test/loopgo.go
@@ -0,0 +1,10 @@
+package main
+
+func bulkSend(numMessages int) float64 {
+	totalCost := 0.0  
+	for i := 0; i < numMessages;i++ {
+		fee := float64(i) * 0.01
+		totalCost += 1.0 + fee
+	}
+	return totalCost
+}
