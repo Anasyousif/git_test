08cc7843fed2db969d8694156a001030be595355
Anas Yousif <Anasyousif182@gmail.com>
2026-08-16 07:03:17 -0400

    daycosts

---
+++ dayvar.go
@@ -0,0 +1,16 @@
+package main
+
+type cost struct {
+	day   int
+	value float64
+}
+
+func getDayCosts(costs []cost, day int) []float64 {
+	dayCosts := []float64{}
+	for _, c := range costs {
+		if c.day == day {
+			dayCosts = append(dayCosts,c.value)
+		}
+	}
+	return dayCosts
+}
