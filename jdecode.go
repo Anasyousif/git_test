42168d8574fec626ef6a1a569abe11def4d345d8
Anas Yousif <Anasyousif182@gmail.com>
2026-08-30 06:25:25 -0400

    issues

---
+++ jdecode.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+func getIssues(url string) ([]Issue, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+
+	var issues []Issue
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&issues)
+	if err != nil {
+		return nil, err
+	}
+	return issues,nil
+}
