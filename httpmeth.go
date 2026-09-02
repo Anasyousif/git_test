1873bd2a781b45b0b97c93661b8584f6fefcb964
Anas Yousif <Anasyousif182@gmail.com>
2026-09-02 07:59:19 -0400

    getmethods

---
+++ httpmeth.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func getUsers(url string) ([]User, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil,err
+	}
+	defer resp.Body.Close()
+	var users []User 
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&users)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
